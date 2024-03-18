6ad2d59dcfef41138ab28713bc472414bbf18fdd
MichaelHowe11 <michael.howell23@student.cart.org>
2024-03-18 15:46:44 -0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
 
     // ascii art made with http://www.network-science.de/ascii/
     const title =
-    `                                                                                                                                                                                        
+        `                                                                                                                                                                                        
      _____ _     _        _        _   _                 __   __             ____  _          _                                                                                      
     |_   _| |__ (_)___   (_)___   | | | | _____      __  \\ \\ / /__  _   _   |  _ \\(_) ___  __| |                                                                                     
       | | | '_ \\| / __|  | / __|  | |_| |/ _ \\ \\ /\\ / /   \\ V / _ \\| | | |  | | | | |/ _ \\/ _\` |                                                                                     
@@ -13,20 +16,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
     console.log(title + `
 Choose Your Occupation & Starting Position
-Unemployed - A House in Muldraugh
 High School Student - Muldraugh High
 Fast Food Employee - Spiffo's (An American Fast Food Restaurant)
+Unemployed - A House in Muldraugh
+
 
 Type 'help' for instructions on how to play.`)
 
-
+    let occupation = 0;
 
     let y = 0;
     let x = 0;
 
-    function moveNorth(params) {
 
-    }
+
+
+
+
 
     document.getElementById('userInputForm').addEventListener('submit', function (eventData) {
         eventData.preventDefault();
@@ -54,9 +60,24 @@ To pick up items type 'pickup' and for dropping items type 'drop (item name)'.
             
 To see your inventory type 'inventory'
             
-To attack an enemy type '(weapon of choice) attack (enemy name)'`)
+To attack an enemy type '(weapon of choice) attack (enemy name)'
+
+Also all text input is NOT case sensitive.`)
         }
 
+        // occupation 1 is high school student, occupation 2 is fast food employee, and occupation 3 is unemployed.
+        if (occupation = 0) {
+            if (input === "high school student") {
+                console.log("Your occupation is a High School Student.")
+                occupation = 1;
+            } else if (input === "fast food employee") {
+                console.log("Your occupation is a Fast Food Employee")
+                occupation = 2;
+            } else if (input === "unemployed") {
+                console.log("You are unemployed, you should get a job you lazy bum! I guess it's too late now huh.")
+                occupation = 3;
+            }
+        }
 
 
 
