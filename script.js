0bce84911af6f0d5b102f0038e152fdaae4acf99
MichaelHowe11 <michael.howell23@student.cart.org>
2024-03-18 14:35:38 -0700

    Summary
    random7

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-console.log(`This is How You Died...
-Type 'North', 'West', 'East', or 'South' to move.
-To pick up items type 'pickup' and for dropping items type 'drop (item name)'.
-To see your inventory type 'inventory'`)
+
+    // ascii art made with http://www.network-science.de/ascii/
+    const title =
+    `                                                                                                                                                                                        
+     _____ _     _        _        _   _                 __   __             ____  _          _                                                                                      
+    |_   _| |__ (_)___   (_)___   | | | | _____      __  \\ \\ / /__  _   _   |  _ \\(_) ___  __| |                                                                                     
+      | | | '_ \\| / __|  | / __|  | |_| |/ _ \\ \\ /\\ / /   \\ V / _ \\| | | |  | | | | |/ _ \\/ _\` |                                                                                     
+      | | | | | | \\__ \\  | \\__ \\  |  _  | (_) \\ V  V /     | | (_) | |_| |  | |_| | |  __/ (_| |_ _ _                                                                               
+      |_| |_| |_|_|___/  |_|___/  |_| |_|\\___/ \\_/\\_/      |_|\\___/ \\__,_|  |____/|_|\\___|\\__,_(_|_|_)                                                                              
+    `;
+
+    console.log(title + `
+Choose Your Occupation & Starting Position
+Unemployed - A House in Muldraugh
+High School Student - Muldraugh High
+Fast Food Employee - Spiffo's (An American Fast Food Restaurant)
+
+Type 'help' for instructions on how to play.`)
 
 
 
@@ -11,27 +25,37 @@ To see your inventory type 'inventory'`)
     let x = 0;
 
     function moveNorth(params) {
-        
+
     }
 
     document.getElementById('userInputForm').addEventListener('submit', function (eventData) {
         eventData.preventDefault();
+
         let input = document.getElementById("userInput").value;
+        document.getElementById('userInput').value = '';
         input = input.toLowerCase()
 
         console.log(input)
 
         if (input === "north") {
-            y+=1
+            y += 1
         } else if (input === "west") {
-            x-=1
+            x -= 1
         } else if (input === "east") {
-            x+=1
+            x += 1
         } else if (input === "south") {
-            y-=1
+            y -= 1
         }
 
-
+        if (input === "help") {
+            console.log(`Type 'North', 'West', 'East', or 'South' to move.
+            
+To pick up items type 'pickup' and for dropping items type 'drop (item name)'.
+            
+To see your inventory type 'inventory'
+            
+To attack an enemy type '(weapon of choice) attack (enemy name)'`)
+        }
 
 
 
